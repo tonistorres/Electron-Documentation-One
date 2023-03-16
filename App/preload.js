4fc49f41b45ced4094378ab74e3bb469e194206c
tonistorres <sisvenda2011@gmail.com>
2023-03-16 10:20:51 -0300

    feat: adding content to files

---
+++ App/preload.js
@@ -0,0 +1,13 @@
+/**
+ *É aqui que anexar um script de pré-carregamento ao seu renderizador é útil. Um script de pré-carregamento é executado antes que o processo do renderizador seja carregado e tem acesso aos globais do renderizador (por exemplo, windowe document) e a um ambiente Node.js.
+ */
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
+  }
+})
